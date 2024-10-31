930790a8618898b3df5d84116cafcc7464e0b887
Raavi Kaur <raavi211299@gmail.com>
2024-10-30 23:07:31 -0500

    add js file and link in the indx file also link jquery

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  randomNumber = Math.floor(Math.random() * 4);
+  var randomChoosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChoosenColour);
+  $("#" + randomChoosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+}
