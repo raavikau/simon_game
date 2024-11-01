e78f0617a0c5d0ef2f0477cacb2e08295e69ecb9
Raavi Kaur <raavi211299@gmail.com>
2024-10-31 21:25:56 -0500

    add playsound function and add animation code on user click

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,20 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
 
 function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
@@ -9,6 +24,10 @@ function nextSequence() {
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  var audio = new Audio("sounds/" + randomChoosenColour + ".mp3");
+  playSound(randomChoosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
