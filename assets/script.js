1f9231f38a56c6c9e8120d63bd3d34cb6a5d86a1
Raavi Kaur <raavi211299@gmail.com>
2024-11-01 12:40:07 -0500

    add function to check user answer against sequence

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,43 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keypress(function (event) {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("#level-title").text("Game Over, Press any Key to Restart");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+  }
+}
 
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
@@ -17,9 +47,15 @@ function animatePress(currentColour) {
 }
 
 function nextSequence() {
-  randomNumber = Math.floor(Math.random() * 4);
+  userClickedPattern = [];
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChoosenColour = buttonColors[randomNumber];
   gamePattern.push(randomChoosenColour);
+
   $("#" + randomChoosenColour)
     .fadeIn(100)
     .fadeOut(100)
